ad84d887eeecc97c95def5239ff23f1d76c7f6a3
Randy Madsen <randy.madsen@utah.edu>
2015-03-23 15:57:29 -0600

    Demo changes.

--- further-i2b2/demo/i2b2-webclient/i2b2config.ini.php
+++ further-i2b2/demo/i2b2-webclient/i2b2config.ini.php
@@ -3,22 +3,23 @@
 
 ;Audit Configuration
 audit.oracle.user = FRTHR_FQE
-audit.oracle.password = @FRTHR_FQE@
-audit.oracle.connectionString = //localhost:1521/xe
+audit.oracle.password = @FURTHER_FQE@
+;audit.oracle.connectionString = //host.does.not.exist:1521/orcl.sid
+audit.oracle.connectionString = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = @DB_HOST@)(PORT = 1521)))(CONNECT_DATA=(SID=FURTHER)))"
 
 ;SSO Configuration
 oracle.user = I2B2USER
 oracle.password = @I2B2USER@
-oracle.connectionString = //localhost:1521/xe
-;cas.url = test.go.utah.edu
-cas.enabled=false
+;oracle.connectionString = //host.does.not.exist:1521/orcl.sid
+oracle.connectionString = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = @DB_HOST@)(PORT = 1521)))(CONNECT_DATA=(SID=FURTHER)))"
+cas.url = @CAS_HOST@
+cas.enabled = true
 sso.hmac.secret = @SSO_HMAC_SECRET@
 
 ;Data Export Configuration
-csv.export.url = http://localhost:9000/fqe/rest/fqe/query/export/CSV
-
-;Demo Configuration
-demo=true
+csv.export.url = http://@ESB_HOST@:9000/fqe/rest/fqe/query/export/CSV
 
 ;CSRF Protection
 nonce.secret=@NONCE_SECRET@
+
+demo = true
